3aa32b82161b97312e7ab53955143aaab27e2f45
박현우 <65493966+hyunwoo6321@users.noreply.github.com>
2023-02-12 00:34:42 +0900

    대?충 만든 이미지 슬라이드 푸시

---
+++ imageslide.js
@@ -0,0 +1,36 @@
+let index = 0;
+window.onload = function(){
+    slideShow();
+}
+
+
+function previous() {
+    const x = document.getElementsByClassName("slide");
+    let i;
+    index--;
+    if (index <= 0) {
+        index = x.length;
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[index - 1].style.display = "block";
+}
+
+function next() {
+    const x = document.getElementsByClassName("slide");
+    let i;
+    index++;
+    if (index > x.length) {
+        index = 1;
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[index - 1].style.display = "block";
+}
+
+function slideShow() {
+    next()
+    setTimeout(slideShow, 4000);
+}
