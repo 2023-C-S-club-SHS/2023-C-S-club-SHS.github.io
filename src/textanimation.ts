5839c584d6257a1834ef26eb51da86fc52e055fa
hyunwoo6321 <phw1508@naver.com>
2023-03-04 22:58:15 +0900

    거지같은 애플 글씨 초안

--- src/textanimation.ts
+++ src/textanimation.ts
@@ -1,5 +1,5 @@
-const content1 = (document.getElementById("content1") as HTMLDivElement);
+const gradient = (document.getElementById("content-gradient2") as HTMLDivElement);
 
-window.onscroll = function () {
-    
+window.onscroll = function() {
+    gradient.style.transform = "translateY(-" + window.scrollY + "px)" + "scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)"
 }
