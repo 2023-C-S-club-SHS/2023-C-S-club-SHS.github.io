4b293990dd402ef8a7341e237697ecd3c3f18eb2
박현우 <65493966+hyunwoo6321@users.noreply.github.com>
2023-03-05 00:48:14 +0900

    애플 글씨 업데이트

--- src/textanimation.ts
+++
@@ -1,5 +0,0 @@
-const gradient = (document.getElementById("content-gradient2") as HTMLDivElement);
-
-window.onscroll = function() {
-    gradient.style.transform = "translateY(-" + window.scrollY + "px)" + "scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)"
-}
