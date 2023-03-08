ffdc2bd9e14955fd658206c8465cd50a6a9d1778
hyunwoo6321 <phw1508@naver.com>
2023-03-08 19:54:27 +0900

    qr 예쁘게 수정

--- qr_generate.py
+++ qr_generate.py
@@ -6,18 +6,13 @@ from qrcode.image.styles import moduledrawers, colormasks
 
 thumb_img_path = 'resources/clublogo_background.png'
 thumb_img = Image.open(thumb_img_path)
-thumb_img.thumbnail((100, 100))
 
 qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
 qr.add_data('https://2023shscsclub.github.io/')
 qr.make()
 
 qr_info = qr.make_image(image_factory=StyledPilImage,
-                        module_drawer=moduledrawers.CircleModuleDrawer(),
-                        color_mask=colormasks.RadialGradiantColorMask(
-                            edge_color=(104,221,168),
-                            center_color=(222,62,102))).convert('RGB')
-pos = ((qr_info.size[0]-thumb_img.size[0]) // 2, (qr_info.size[1]-thumb_img.size[1])//2)
-qr_info.paste(thumb_img, pos)
+                        module_drawer=moduledrawers.RoundedModuleDrawer(),
+                        color_mask=colormasks.ImageColorMask(color_mask_image=thumb_img)).convert('RGB')
 
 qr_info.save('qrcode.png')
